a20afaa32fde1afec221d90de2e5d30a4f917554
Mugara250 <mbonyumugara52@gmail.com>
2025-09-08 16:37:22 +0200

    modules and exports

---
+++ modules.js
@@ -0,0 +1,4 @@
+const foods = ["cabbage", "carrots", "potatoes", "pineapple"];
+const utilities = ["electricity", "water", "cooking gas", "fuel"];
+console.log(foods);
+module.exports = {foods, utilities};
---
+++ useModules.js
@@ -0,0 +1,5 @@
+const {foods, utilities} = require("./modules");
+console.log(foods, utilities);
+
+const os = require("os");
+console.log(os.platform(), os.homedir());
