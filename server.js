b7178336526ed3bafcaa9980316c0294d0c30f8d
Mugara250 <mbonyumugara52@gmail.com>
2025-09-12 14:56:51 +0200

    working with npm

--- server.js
+++ server.js
@@ -1,43 +1,42 @@
 const http = require("http");
 const fs = require("fs");
+const _ = require("lodash");
 // create the server
 const server = http.createServer((request, response) => {
-    console.log(request.url, request.method);
+  const num = _.random(0, 20);
+  console.log(num);
+  let path = "./views/";
+  switch (request.url) {
+    case "/":
+      path += "index.html";
+      response.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      response.statusCode = 200;
+      break;
+    case "/about-me":
+      response.statusCode = 301;
+      response.setHeader("Location", "/about");
+      break;
+    default:
+      path += "error.html";
+      response.statusCode = 404;
+  }
 
-    let path = "./views/";
-    switch (request.url) {
-        case "/":
-            path += "index.html";
-            response.statusCode = 200;
-            break;
-        case "/about":
-            path += "about.html";
-            response.statusCode = 200;
-            break;
-        default:
-            path += "error.html";
-            response.statusCode = 404;
+  response.setHeader("Content-Type", "text/html");
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      response.end();
+    } else {
+      response.end(data);
     }
-
-    response.setHeader("Content-Type", "text/html");
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            response.end();
-        } else {
-            response.end(data);
-        }
-    })
+  });
 });
 
 const PORT = 3000;
 // starting the server
 server.listen(PORT, "localhost", () => {
-    console.log(`Server has started listening on port ${PORT}`);
-})
-
-const numbers = [1, 2, 3, 4, 5];
-
-for (let i = 0; i < numbers.length; i++) {
-console.log(numbers[i]);
-}
+  console.log(`Server has started listening on port ${PORT}`);
+});
