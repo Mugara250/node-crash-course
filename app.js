4272663f308ac43390bfc6da530a11a71df6cd2c
Mugara250 <mbonyumugara52@gmail.com>
2025-09-13 21:41:57 +0200

    view engines

--- app.js
+++ app.js
@@ -3,24 +3,31 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // starting the server
 app.listen(3000);
 
 // listening for and responding to requests
 app.get("/", (request, response) => {
-  response.sendFile("./views/index.html", { root: __dirname });
+    const blogs = [
+        {title: "Yoshi finds eggs", snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: "Mario finds stars", snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: "How to defeat bowser", snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ]
+  response.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (request, response) => {
-  response.sendFile("./views/about.html", { root: __dirname });
+  response.render("about", { title: "About" });
 });
 
-// redirects
-app.get("/about-me", (request, response) => {
-  response.redirect("/about");
+app.get("/blogs/create", (request, response) => {
+  response.render("create", { title: "Create" });
 });
 
 // 404 error
 app.use((request, response) => {
-  response.status(404).sendFile("./views/error.html", { root: __dirname });
+  response.status(404).render("error");
 });
